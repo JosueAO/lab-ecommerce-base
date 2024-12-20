cf936f541a1103964ebdd1bc7a1461da54a24645
Josue A. Oliveira <josueao.mobile@gmail.com>
2024-12-20 00:33:39 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/josueao/labcommercebase/LabcommercebaseApplication.java
@@ -0,0 +1,13 @@
+package com.josueao.labcommercebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabcommercebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabcommercebaseApplication.class, args);
+	}
+
+}
